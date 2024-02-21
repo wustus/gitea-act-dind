54ba559730193201bdccfbadf83cfc25386b6dbd
Justus Stahlhut <justus.stahlhut@realestatepilot.com>
2024-02-21 11:58:24 +0100

    log mounts

--- pkg/container/docker_run.go
+++ pkg/container/docker_run.go
@@ -887,7 +887,7 @@ func (cr *containerReference) start() common.Executor {
 		logger := common.Logger(ctx)
 		logger.Debugf("Starting container: %v", cr.id)
 
-		logger.Info(cr.input)
+		logger.Info(cr.input.Mounts)
 
 		if err := cr.cli.ContainerStart(ctx, cr.id, types.ContainerStartOptions{}); err != nil {
 			return fmt.Errorf("failed to start container: %w", err)
